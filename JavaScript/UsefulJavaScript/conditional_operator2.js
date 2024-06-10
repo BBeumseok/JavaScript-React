2fc2552791da6e5cbe2e21d2f039a36a3f0a8625
BBeumseok <bbseok97@naver.com>
2024-06-10 12:21:20 +0900

    240610 3교시

--- JavaScript/UsefulJavaScript/conditional_operator2.js
+++ JavaScript/UsefulJavaScript/conditional_operator2.js
@@ -15,3 +15,59 @@ const isAnimal = (text) => ['고양이', '개', '거북이', '너구리'].includ
 
 console.log(isAnimal('개'));
 console.log(isAnimal('노트북'));
+
+//  값에 따라 다른 결과물을 반환해야 할 경우
+function getSound(animal) {
+  //  1.
+  // if (animal === '개') return '멍멍!';
+  // if (animal === '고양이') return '야옹~!';
+  // if (animal === '참새') return '짹짹!';
+  // if (animal === '비둘기') return '구구~~';
+  // return '...?';
+  //  2.
+  // switch (animal) {
+  //   case '개':
+  //     return '멍멍!';
+  //   case '고양이':
+  //     return '야옹~!';
+  //   case '참새':
+  //     return '짹짹 !!';
+  //   case '비둘기':
+  //     return '구구~~';
+  //   default:
+  //     return '...?';
+  // }
+  //  3.
+  const sounds = {
+    개: '멍멍',
+    고양이: '야옹',
+    참새: '짹짹',
+    비둘기: '구구',
+  };
+  return sounds[animal] || '...?';
+}
+
+//  실행해야 하는 코드 구문이 다를 경우
+function makeSound(animal) {
+  const tasks = {
+    개() {
+      console.log('멍멍 !!');
+    },
+    고양이() {
+      console.log('야옹~!');
+    },
+    참새() {
+      console.log('짹짹 !@');
+    },
+    비둘기() {
+      console.log('구구~~');
+    },
+  };
+  if (!tasks[animal]) {
+    console.log('...?');
+    return;
+  }
+  tasks[animal]();
+}
+makeSound('개');
+makeSound('비둘기');
