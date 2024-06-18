1eeb8369734c3e54d420b11c8315b9fbfd1b763d
BBeumseok <bbseok97@naver.com>
2024-06-18 12:18:54 +0900

    240618 3교시

---
+++ JavaScript/mashup-todolist/src/components/TodoCreate.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import Styled, { css } from 'styled-components';
+import { MdAdd } from 'react-icons/md';
+
+const CircleButton = Styled.button`
+    background: #38d9a9;
+    &:hover{
+        background: #63e6be;
+    }
+    &:active{
+        background: #20c997;
+    }
+    
+    //해당내용을 우선시해서 올려준다
+    z-index:5;
+    cursor:pointer;
+    width: 80px;
+    height: 80px;
+    display:block;
+    align-items:center;
+    justify-content:center;
+    font-size:60px;
+    position:absolute;
+    left:50%;
+    bottom:0px;
+    transform:translate(-50%,50%);
+    color:white;
+    border-radius:50%;
+    border:none;
+    outline:none;
+    display:flex;
+    align-items:center;
+
+    transition: 0.125s all ease-in;
+    ${props =>
+      props.open &&
+      css`
+        background: #ff6b6b;
+        &:hover {
+          background: #ff8787;
+        }
+        &:active {
+          background: #fa5252;
+        }
+          transform: translate(-50%, 50%) rotate(45deg); /* deg: degree 약어 */
+      `
+    }
+`;
+const InsertFormPositioner = Styled.div`
+    width:100%;
+    bottom:0;
+    position:absolute;
+`;
+
+const InsertForm = Styled.div`
+    background: #f8f9fa;
+    padding-left:32px;
+    padding-top:32px;
+    padding-right:32px;
+    padding-bottom:72px;
+    border-bottom-left-radius:16px;
+    border-bottom-right-radius:16px;
+    border-top:1px solid #e9ecef;
+`;
+
+const Input = Styled.input`
+    
+`;
+
+function TodoCreate(){
+
+    //TodoCreate 컴포넌트 상태
+    const [open,setOpen] = useState(false);
+    const onToggle = () => setOpen(!open);
+
+    return(
+        <>
+            {open && (
+                <InsertFormPositioner>
+                    <InsertForm>
+                        <Input autoFocus placeholder="할 일을 입력 후, Enter를 누르세요"/>
+                    </InsertForm>
+                </InsertFormPositioner>
+            )}
+            <CircleButton onClick={onToggle} open={open}>
+                <MdAdd />
+            </CircleButton>
+        </>
+    )
+}
+export default TodoCreate;
---
+++ JavaScript/mashup-todolist/src/components/TodoItem.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { MdDelete, MdDone } from 'react-icons/md';
+
+//  삭제
+const Remove = styled.div`
+  display: flex;
+  align-items: center;  /* 상.하 가운데 정렬 */
+  justify-content: center;  /* 좌우 가운데 정렬 */
+  color: #dee2e6;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    color: #ff6b6b;
+  }
+  display: none;
+`;
+
+//  todoItem
+const TodoItemBlock = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 12px;
+  padding-bottom: 12px;
+
+  &:hover {
+    ${Remove} {
+      display: initial;
+    }
+  }
+`;
+
+//  checkbox
+const CheckCircle = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+  border: 1px solid #ced4da;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;   /* 체크서클 오른쪽으로 마진 */
+  cursor: pointer;
+  ${props =>
+    props.done && 
+    css`
+      border: 1px solid #38d9a9;
+      color: #38d9a9;
+    `
+  }
+`;
+
+//  text 영역
+const Text = styled.div`
+  flex: 1;
+  font-size: 21px;
+  color: #495057;
+  ${props =>
+    props.done &&
+    css`
+      color: #ced4da;
+    `
+  }
+`;
+
+function TodoItem({id, done, text}) {
+  return (
+  <TodoItemBlock>
+    <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+    <Text done={done}>{text}</Text>
+    <Remove>
+      <MdDelete></MdDelete>
+    </Remove>
+  </TodoItemBlock>
+  );
+}
+
+export default TodoItem;
+
